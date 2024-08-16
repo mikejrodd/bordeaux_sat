99d3ef3b0c3c8a3c18bff4c85b0bd1e5731ff803
Mike <137613726+mikejrodd@users.noreply.github.com>
2024-08-16 09:58:07 +0200

    Update update-tle.js

--- data/tle/update-tle.js
+++ data/tle/update-tle.js
@@ -8,12 +8,6 @@ const process = require("process");
 process.chdir(__dirname);
 
 function downloadTLE(groupName) {
-  if (groupName === "Bordeaux") {
-    // Skip download for Bordeaux as it's manually created
-    console.log(`Skipping download for ${groupName}.txt as it is manually created.`);
-    return;
-  }
-
   const url = `https://celestrak.org/NORAD/elements/gp.php?GROUP=${groupName}&FORMAT=tle`;
   const path = "groups/";
   const filename = `${groupName}.txt`;
@@ -28,26 +22,63 @@ function downloadTLE(groupName) {
   });
 }
 
-// Groups to download TLEs for
+// https://celestrak.org/NORAD/elements/
+// [...document.links].filter((link) => link.href.match(/gp.php\?GROUP=/)).map((link => link.href.match(/GROUP=(?<name>.*)&FORMAT/).groups.name));
 const groups = [
   "last-30-days",
   "stations",
+  // "visual",
   "active",
+  // "analyst",
+  // "1982-092",
+  // "1999-025",
+  // "iridium-33-debris",
+  // "cosmos-2251-debris",
   "weather",
+  // "noaa",
+  // "goes",
   "resource",
+  // "sarsat",
+  // "dmc",
+  // "tdrss",
+  // "argos",
   "planet",
   "spire",
+  // "geo",
+  // "intelsat",
+  // "ses",
+  // "iridium",
   "iridium-NEXT",
   "starlink",
   "oneweb",
+  // "orbcomm",
   "globalstar",
+  // "swarm",
+  // "amateur",
+  // "x-comm",
+  // "other-comm",
+  // "satnogs",
+  // "gorizont",
+  // "raduga",
+  // "molniya",
   "gnss",
+  // "gps-ops",
+  // "glo-ops",
+  // "galileo",
+  // "beidou",
+  // "sbas",
+  // "nnss",
+  // "musson",
   "science",
+  // "geodetic",
+  // "engineering",
+  // "education",
+  // "military",
+  // "radar",
   "cubesat",
-  "Bordeaux",  // Custom Bordeaux group
+  // "other",
 ];
 
 groups.forEach((group) => {
   downloadTLE(group);
 });
-
